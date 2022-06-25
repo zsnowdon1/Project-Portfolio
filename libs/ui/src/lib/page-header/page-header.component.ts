ab25f0e4b96010e8081bf1e8e8dec5df610433c5
zlsnowdo <zlsnowdo@ncsu.edu>
2022-06-25 14:13:15 -0500

    Added first news-compiler application

---
+++ libs/ui/src/lib/page-header/page-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'project-portfolio-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.css'],
+})
+export class PageHeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ libs/ui/src/lib/ui.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageHeaderComponent } from './page-header/page-header.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [PageHeaderComponent],
+})
+export class UiModule {}
