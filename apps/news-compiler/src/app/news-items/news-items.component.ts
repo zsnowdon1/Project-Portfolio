51495355e932cd608b5432824b52f34865d42954
zlsnowdo <zlsnowdo@ncsu.edu>
2022-06-29 14:42:33 -0500

    Change UI of two nba stories

--- apps/news-compiler/src/app/news-items/news-items.component.ts
+++ apps/news-compiler/src/app/news-items/news-items.component.ts
@@ -17,18 +17,20 @@ export class NewsItemsComponent implements OnInit {
     this.stories = new Array<NewsItem>();
 
     const NewsItem1 = {
-      title: "Title",
-      subtitle: "sub",
-      url: "google.com"
+      title: "How Kyrie Irving's opt-in answers one question for the Nets and creates many more",
+      subtitle: "Despite Kyrie Irving deciding to opt in with the Nets, there are still plenty of ways this relationship could play out.",
+      url: "https://a4.espncdn.com/combiner/i?img=%2Fphoto%2F2022%2F0310%2Fr984466_1296x729_16%2D9.jpg&w=238&h=133&scale=crop&cquality=80&location=center"
     } as NewsItem
 
     const NewsItem2 = {
-      title: "Title 2",
-      subtitle: "sub 2",
-      url: "google.com"
+      title: "The Knicks might throw big money at free agent Jalen Brunson. Should they?",
+      subtitle: "Jalen Brunson became a star playing next to Luka Doncic. Here's what a change of scenery could mean for the coveted free-agent guard.",
+      url: "https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2022%2F0627%2Fr1029766_1296x729_16%2D9.jpg&w=238&h=133&scale=crop&cquality=80&location=center"
     } as NewsItem
+
     this.stories.push(NewsItem1);
     this.stories.push(NewsItem2);
+
   }
 
 }
